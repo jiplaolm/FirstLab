559bb956b5d2741d7454a6321f71d8da0f32d73e
Mikel Larrañaga <mikel.larranaga@ehu.eus>
2018-01-30 11:33:19 +0100

    Beste bat

--- SupermercadoJava8/src/packSupermercado/ProgramaPrincipal.java
+++ SupermercadoJava8/src/packSupermercado/ProgramaPrincipal.java
@@ -22,7 +22,7 @@ public class ProgramaPrincipal {
 		}
 		
 		// TODO: Implementar el mismo comportamiento con Java 8
-		System.out.println("Hello");
+		System.out.println("kkk");
 		
 		// TODO: Obtener el precio del producto más barato por sección
 		
